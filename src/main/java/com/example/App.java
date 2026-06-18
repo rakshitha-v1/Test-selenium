f6d03fc769001244eb0b73ce68e831b917fedd5e
rakshitha-v1 <rakshithashyam2006@gmail.com>
2026-06-18 06:58:57 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,11 +2,15 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.chrome.ChromeOptions;
 public class App{
 	public static void main(String[] args){
-		
-		WebDriver driver=new ChromeDriver();
+		ChromeOptions options=new ChromeOptions();
+		options.addArguments("--headless=new");
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--disable-gpu");
+		WebDriver driver=new ChromeDriver(options);
 		driver.get("https://www.saucedemo.com");
 		driver.manage().window().maximize();
 		driver.findElement(By.id("user-name")).sendKeys("standard_user");
